4cec14fcf0cf4eb3b8577008a491d8f3007f32ce
DH836 <75822367+DH836@users.noreply.github.com>
2020-12-14 04:04:28 -0800

    commit

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 var btnTranslate=document.querySelector("#btn-translate");
-var txtInput=document.querySelector("#txt-input");
+var txtInput=document.querySelector("#txt-input"); 
+var outputDiv=document.querySelector("#output");
 
-console.log(txtInput);
+var serverURL= "https://api.funtranslation.com/translate/minion.json"
+function getTranslationURL(text){
+    return serverURL + "?" +"text" + text 
+}
+function errorHandler(error){
+    console.log("erroe occured", error);
+    alert("something wrong with server! try again after some time")
+}
 
 function clickHandler(){
-    console.log("clicked");
-    console.log("input",txtInput.value);
+var inputText=txtInput.Value;
+
+fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json =>{
+        var translatedText=json.contents.translated;
+        outputDiv.innerText=translatedText;
+    })
+    .catch(errorHaandler)
 };
+
 btnTranslate.addEventListener("click",clickHandler)
